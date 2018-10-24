afabc3b9b4937287fe9f1f42bdf05a787c106a81
Lupiitha10 <al221610526@gmail.com>
2018-10-24 17:30:54 -0500

    Form Obras Update

--- resources/views/sistema/nueva_obra.blade.php
+++ resources/views/sistema/nueva_obra.blade.php
@@ -3,14 +3,14 @@
 	<head>
 		<title>Alta Obras | Casa Blanca</title>
 		<meta charset="utf-8" />
-		<link rel="stylesheet" type="text/css" href="../CSS/forms.css" />
+		  <link rel="stylesheet" href="{{ asset('assets/css/forms.css') }}" />
 		<link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet" />
 	</head>
 	<body>
 		<div id="altas_user-div">
 			<form id="altas_user-form" action="{{route ('guardaobra') }}" method = 'POST' enctype='multipart/form-data'>
 				<h2 id="form-title">Nueva Obra</h2>
-				<input type="text" placeholder="ID de Obra" name="ido" disabled /><br></br>
+				<input type="text" placeholder="ID de Obra" name="ido" disabled  value="{{$idof}}" /><br></br>
 				<input type="text" placeholder="Nombre" name="nombre" /><br></br>
 				<input type="text" placeholder="Duaración" name="duracion" /><br></br>
 				<input type="text" placeholder="Cupo" name="cupo" /><br></br>
