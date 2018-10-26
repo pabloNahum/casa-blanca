300a9f056bea9a2eb3c5c1fd6940b4f17483a5d3
urielsem <al22160531@gmail.com>
2018-10-26 12:10:15 -0500

    update estilos consultas

--- resources/views/sistema/consulta_cliente.blade.php
+++ resources/views/sistema/consulta_cliente.blade.php
@@ -1,15 +1,32 @@
 <html>
+<head>
+<meta charset="utf-8" />
+<title>Consulta clientes| Casa Blanca</title>
+	<script src="http://code.jquery.com/jquery-latest.min.js"type="text/javascript"></script>
+    <link rel="stylesheet" href="{{ asset('assets/tabla/demo_table.css') }}" />
+	 <script type="text/javascript" language="javascript" src="{{ asset('assets/tabla/jquery.dataTables.js')}}"></script>
+	<script type="text/javascript" language="javascript" src="{{ asset('assets/tabla/jslistadopaises.js')}}"></script>
+</head>
 <body>
-	<h1>Consulta Clientes</h1>
-	<br>
-	<table border="1">
+	<h1 align="center">Consulta Clientes</h1>
+<article id="contenido">
+<table cellpadding="0" cellspacing="0" border="0" class="display" id="tabla_lista_paises">
+<thead>
 		<tr>
-			<td>Clave</td>
-			<td>Nombre</td>
-			<td>Apellido Paterno</td>
-			<td>Apellido Materno</td>
-			<td>Telefono</td>
+			<th>Clave</th>
+			<th>Nombre</th>
+			<th>Apellido Paterno</th>
+			<th>Apellido Materno</th>
+			<th>Telefono</th>
 		</tr>
+</thead>
+<tfoot>
+         <tr>
+             <th></th>
+             <th></th>                                        
+         </tr>
+</tfoot>
+<tbody>		
 			@foreach($clientes as $cte)
 		<tr>
 			<td align="center">{{$cte->idc}}</td>
@@ -19,6 +36,8 @@
 			<td>{{$cte->telefono}}</td>
 		</tr>
 			@endforeach
+</tbody>
 	</table>
+	</article>
 	</body>
 	</html>
